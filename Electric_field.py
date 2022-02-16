d88ce7cf6dd72794e674ab7659c4b6f10bfbeb91
Matías Liempi <71166666+liempi@users.noreply.github.com>
2022-02-16 16:47:07 -0300

    Update Electric_field.py
    Now Electric Charges are defined such as class object. 
    I fixed an error with the vector field when the calculus was made in the same coord where the charges are positioned.

--- Electric_field.py
+++ Electric_field.py
@@ -1,48 +1,92 @@
 from manim import *
 import numpy as np
 
-class electric_field(Scene):
+
+#Definition of electric charge and their properties in two dimensions (position, color and Q value)
+
+class charge_2D:
+    def __init__(self , x_pos, y_pos,Q,color):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.Q     = Q
+        self.color = color
+        
+#Scene configuration (Elements, animations)
+        
+class electric_field_2D(Scene):
+
     def construct(self):
+    
         #Setting the grid
-        grid = NumberPlane(color=BLUE,background_line_style={
-                "stroke_color": WHITE,
-                "stroke_opacity": 0.1}
-                )
+        grid = NumberPlane(color=BLUE , background_line_style = {"stroke_color": WHITE, "stroke_opacity": 0.1})
+        
         #Adding axis labels
         grid.add(grid.get_axis_labels())
         
-        #Charge definition, if carga<0 color: BLUE  elif carga>0 color:RED
-        def charge(color,x_0):
-            if color == RED:
-                dot = Dot(radius=0.05,color=PURE_RED).shift(np.array([x_0[0],x_0[1],x_0[2]]))
-            elif color == BLUE:
-                dot = Dot(radius=0.1,color=PURE_BLUE).shift(np.array([x_0[0],x_0[1],x_0[2]]))
-            return dot
-            
-        #Coordinates of charge
-        x_0 = np.array([1,0,0])
-
         #Avoid zero division error
-        eps=0.000001
+        eps = 0.000000001
+        
+        #vacuum permittivity
+        eps_0 = 8.8541878176e{-12}
 
-        #Function to calculate the vector field. The first value is the charge in Coulombs.
-        function = lambda x: -200/(4*PI) *np.array([(x[0]-x_0[0])/((x[0]-x_0[0])**2+(x[1]-x_0[1])**2+eps)**(3/2),\
-            (x[1]-x_0[1])/((x[0]-x_0[0])**2+(x[1]-x_0[1])**2+eps)**(3/2)])
+        #Charge parameters input
+        #Color can be PURE_RED(Q>0) or PURE_BLUE(Q<0), you can use other colors
+        charge = charge_2D(0,0,-30,PURE_BLUE)
         
+        charge_2 = charge_2D(2,2,10,PURE_RED)
+
         
-        #Vector field
-        vector_field = ArrowVectorField(function,color_scheme=None,min_color_scheme_value=0,\
-                  max_color_scheme_value=5,color=PURE_BLUE,vector_config={"stroke_width":6})
+        #Representation of charges
+        dot = Dot(radius=0.05,color=charge.color).shift(RIGHT*charge.x_pos+UP*charge.y_pos)
+        
+        dot_2 = Dot(radius=0.05,color=charge_2.color).shift(RIGHT*charge_2.x_pos+UP*charge_2.y_pos)
+
+
+        #Function to calculate the electric field for one charge
+        def function(x):
+            if (x[0] == charge.x_pos and x[1] == charge.y_pos):
+                return [0,0]
+            else:
+                r = ((x[0]-charge.x_pos)**2 + (x[1]-charge.y_pos)**2)**(3/2)
+
+                vectors = -charge.Q/(4*PI*eps_0) * np.array([(x[0]-charge.x_pos)/r, (x[1]-charge.y_pos)/r])
+                return vectors
+        
+        #Example if you want to graph the electric field for two charges
+        def function_2(x):
+        #Here I exclude the arrow near to the charges.
+            if (x[0] == charge.x_pos and x[1] == charge.y_pos) or (x[0] == charge_2.x_pos and x[1] == charge_2.y_pos):
+            
+                return [0,0]
+        #In any other case we calculate the electric field
+            else:
+                r = ((x[0]-charge.x_pos)**2 + (x[1]-charge.y_pos)**2)**(3/2)
+                
+                r_2 = ((x[0]-charge_2.x_pos)**2+(x[1]-charge_2.y_pos)**2)**(3/2)
+                
+                vectors = charge.Q/(4*PI*eps_0)*np.array([(x[0]-charge.x_pos)/r,\
+                (x[1]-charge.y_pos)/r]) +charge_2.Q/(4*PI*eps_0)*np.array([(x[0]-charge_2.x_pos)/r_2,\
+                (x[1]-charge_2.y_pos)/r_2])
+            
+                return vectors
+        #If you want to add more elecrric charges, you need to create a new charge and introduce values for position, color and charge.
+        #Then,you need to add another condition in if and in else as above. Just compare function with function_2.
+ 
+        #Vector field creation in Manim
+        vector_field = ArrowVectorField(function_2,color_scheme=None,\
+        color=PURE_BLUE,vector_config={"stroke_width":6, "max_tip_length_to_length_ratio":0.25})
         
         #Adding the grid
         self.play(Create(grid),run_time=2)
         
         #Adding the charge
-        self.play(Create(charge(RED,x_0)))
+        self.play(Create(dot))
+        
+        self.play(Create(dot_2))
         
         #Adding the vector field
         self.play(Create(vector_field),run_time=6)
         
         #Waiting 2 seconds        
-        self.wait(2)
+        self.wait(5)
         
